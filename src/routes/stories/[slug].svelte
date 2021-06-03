83405b3ab568756cd7d20b7dad236c7584c7b348
Adam Faur <adam.faur@gmail.com>
2021-06-02 22:37:28 -0700

    change ghost_story to accept slug as url get param

--- src/routes/stories/[slug].svelte
+++ src/routes/stories/[slug].svelte
@@ -1,9 +1,8 @@
 <script context="module">
 	export const prerender = true;
   
-	export async function load({ fetch }) {
-      const response = await fetch('/stories/ghost_story');
-	  console.log(response)
+	export async function load({ page, fetch }) {
+      const response = await fetch(`/stories/ghost_story?slug=${page.params.slug}`);
       const { post } = await response.json();
 		
       return {
