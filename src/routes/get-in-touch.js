ab49959695f20a162c21dcb7fce8e542f5de3f45
Edan Ben-Atar <benatare@gmail.com>
2021-06-12 21:50:21 -0400

    env keys update

--- src/routes/get-in-touch.js
+++ src/routes/get-in-touch.js
@@ -1,12 +1,13 @@
 import mailgun from "mailgun-js"
+import dotenv from 'dotenv';
+
+dotenv.config();
+const {DOMAIN} = process.env
+const {MAILGUN_KEY} = process.env
 
 export async function post(request) {
 
-
-    const API_KEY = 'key-0dc454a132e089f79541cc7333589384';
-    const DOMAIN = 'mg.weblime.com';
-
-    const mailgunInstance = mailgun({ apiKey: API_KEY, domain: DOMAIN })
+    const mailgunInstance = mailgun({ apiKey: MAILGUN_KEY, domain: DOMAIN })
     // console.log(request.body.first_name)
     // const body = JSON.parse(request.body)
     const { first_name, last_name } = request.body;
