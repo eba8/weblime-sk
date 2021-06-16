5b68df74a9b1e2bf8f532779607ed0977f8f4b4a
Edan Ben-Atar <benatare@gmail.com>
2021-06-16 11:42:19 -0400

    fix hover button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -50,7 +50,7 @@
         <div class="mt-3 rounded-md sm:mt-0 sm:ml-3">
           <a
             href="/get-in-touch"
-            class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-gray-900 border border-transparent rounded-md hover:bg-gray-200 md:py-4 md:text-lg md:px-10"
+            class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-gray-900 border border-transparent rounded-md hover:bg-gray-800 md:py-4 md:text-lg md:px-10"
           >
             Get In Touch
           </a>
