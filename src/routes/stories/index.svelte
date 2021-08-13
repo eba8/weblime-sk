358653e893028e4c3baf6abedb7471fc3bf66b14
Edan Ben-Atar <benatare@gmail.com>
2021-08-12 22:20:38 -0400

    fix design | move stories

--- src/routes/stories/index.svelte
+++ src/routes/stories/index.svelte
@@ -5,7 +5,6 @@
     const response = await fetch('/stories/ghost_stories');
 
     const { stories } = await response.json();
-    console.log(stories);
     return {
       props: {
         stories,
@@ -19,11 +18,24 @@
 </script>
 
 <div class="relative pb-20 px-4 sm:px-6 lg:pb-28 lg:px-8">
-  <div class="absolute inset-0">
-    <div class="bg-white h-1/3 sm:h-2/3" />
+  <!-- Hero -->
+  <div class=" sm:pt-16 lg:pt-8 lg:pb-14">
+    <div class="px-5 text-center lg:px-5">
+      <h1
+        class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
+      >
+        <span class="block xl:inline">Stories</span>
+      </h1>
+      <p
+        class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+      >
+        We love developing your online presence and equally love to share what
+        we learn along the way.
+      </p>
+    </div>
   </div>
   <div class="relative max-w-7xl mx-auto">
-    <div class="text-center">
+    <!-- <div class="text-center">
       <h2
         class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
       >
@@ -33,7 +45,7 @@
         We love developing your online presence and equally love to share what
         we learn along the way.
       </p>
-    </div>
+    </div> -->
 
     <div
       class="mt-12 max-w-lg mx-auto grid gap-12 lg:grid-cols-3 lg:max-w-none"
@@ -49,7 +61,11 @@
                   {story.reading_time} MIN READ
                 </a>
               </p>
-              <a href="/stories/{story.slug}" class="block mt-2">
+              <a
+                sveltekit:prefetch
+                href="/stories/{story.slug}"
+                class="block mt-2"
+              >
                 <p class="text-xl font-semibold text-gray-900">
                   {story.title}
                 </p>
