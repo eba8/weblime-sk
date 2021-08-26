3577502da633727808ad2f79ee9b97a68de7e5ff
Edan Ben-Atar <benatare@gmail.com>
2021-08-25 22:45:51 -0400

    center text in story cards

--- src/routes/stories/index.svelte
+++ src/routes/stories/index.svelte
@@ -35,31 +35,17 @@
     </div>
   </div>
   <div class="relative max-w-7xl mx-auto">
-    <!-- <div class="text-center">
-      <h2
-        class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
-      >
-        Stories
-      </h2>
-      <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-        We love developing your online presence and equally love to share what
-        we learn along the way.
-      </p>
-    </div> -->
-
     <div
       class="mt-12 max-w-lg mx-auto grid gap-12 lg:grid-cols-3 lg:max-w-none"
     >
       {#each stories as story}
         <div class="flex flex-col bg-gray-100 rounded-md overflow-hidden">
-          <div class="flex-1 p-6 flex flex-col justify-between">
+          <div class="flex-1 p-6 flex flex-col justify-between text-center">
             <div class="flex-1">
               <p
-                class="text-sm font-medium text-primary-600 inline-flex items-center"
+                class="text-sm font-medium text-primary-600 inline-flex items-center "
               >
-                <a href="/stories/{story.slug}" class="hover:underline">
-                  {story.reading_time} MIN READ
-                </a>
+                {story.reading_time} MIN READ
               </p>
               <a
                 sveltekit:prefetch
