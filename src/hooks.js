a40b96599aa13e8a9fe24c30e52c33ec3af4fe29
Edan Ben-Atar <benatare@gmail.com>
2022-01-25 11:13:29 -0500

    roll back adapater and kit versions

--- src/hooks.js
+++ src/hooks.js
@@ -2,33 +2,29 @@ import cookie from 'cookie';
 import { v4 as uuid } from '@lukeed/uuid';
 import urlDictionary from '$lib/urlDictionary';
 
-export const handle = async ({ event, resolve }) => {
-  const cookies = cookie.parse(event.request.headers.cookie || '');
-  event.locals.userid = cookies.userid || uuid();
+export const handle = async ({ request, resolve }) => {
+  const cookies = cookie.parse(request.headers.cookie || '');
+  request.locals.userid = cookies.userid || uuid();
 
-  if (event.request.method) {
-    const method = event.request.method.toUpperCase();
+  // TODO https://github.com/sveltejs/kit/issues/1046
+  if (request.method) {
+    // request.method = request.query.get('_method').toUpperCase();
+    const method = request.method.toUpperCase();
   }
 
-  const response = await resolve(event);
+  const response = await resolve(request);
 
-  if (urlDictionary[event.url]) {
-    return { status: 301, headers: { location: urlDictionary[event.url] } };
+  if (urlDictionary[request.url]) {
+    return { status: 301, headers: { location: urlDictionary[request.url] } };
   }
-
-  // const location = urlDictionary[event.url]
-  // if (location) {
-  //   return { status: 301, headers: { location } };
 
   if (!cookies.userid) {
     // if this is the first time the user has visited this app,
     // set a cookie so that we recognise them when they return
     response.headers[
       'set-cookie'
-    ] = `userid=${event.locals.userid}; Path=/; HttpOnly`;
+    ] = `userid=${request.locals.userid}; Path=/; HttpOnly`;
   }
-
-  //cookie.serialize("userid", event.locals.userid, { path: "/", httpOnly: true })
 
   return response;
 };
