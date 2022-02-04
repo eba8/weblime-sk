f2800316201d56ec2db040aa89b5454e87d9b629
Edan Ben-Atar <benatare@gmail.com>
2022-02-04 16:22:50 -0500

    Revert "update sitemap approach"
    This reverts commit 62dce810d36311ae7165b0f85fa4eb22b568ee0d.

---
+++ src/utils/routelist.js
@@ -0,0 +1,45 @@
+import GhostContentAPI from '@tryghost/content-api'
+import dotenv from 'dotenv';
+
+dotenv.config();
+const {GHOST_KEY} = process.env
+
+async function getPosts() {    
+    const api = new GhostContentAPI({
+        url: 'https://weblime-blog.herokuapp.com',
+        key: GHOST_KEY,
+        version: 'v4'
+    })
+    try {
+        const jsonPosts = await api.posts.browse({limit: 'all'})
+        return jsonPosts
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+/**
+ * This is our build function. It contains the query.
+ * It uses the link resolver to build the routes.
+ * It then writes it to disk in the same dir.
+ */
+const build = async () => {
+    let posts = await getPosts()
+    /**
+     * Svelte uses the `*` to render the other pages that aren't dynamic
+     */
+    let pages = ['*']
+
+    /**
+     * Convert the docs into the related link for Svelte to render.
+     */
+    let result = posts.map(post => `/stories/${post.slug}`);
+
+    pages.push(...result)
+
+    return pages
+}
+
+let pages = await build();
+export default pages
