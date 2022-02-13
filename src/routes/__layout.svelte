1087db5b2ec4c4a6e1186d52bcc3547729de409c
Edan Ben-Atar <benatare@gmail.com>
2022-02-13 08:43:24 -0500

    add clarity

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,6 +12,20 @@
     defer
     data-domain="weblime.com"
     src="https://plausible.io/js/plausible.js"></script>
+  <script type="text/javascript">
+    (function (c, l, a, r, i, t, y) {
+      c[a] =
+        c[a] ||
+        function () {
+          (c[a].q = c[a].q || []).push(arguments);
+        };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = 'https://www.clarity.ms/tag/' + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, 'clarity', 'script', 'am2azj360o');
+  </script>
 </svelte:head>
 
 <main>
