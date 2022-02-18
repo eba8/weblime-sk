31ac0b9273c84593157a569477f11850a1595b9e
Edan Ben-Atar <benatare@gmail.com>
2022-02-18 10:19:27 -0500

    update sitemap

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -94,6 +94,9 @@ const render = () => `<?xml version="1.0" encoding="UTF-8" ?>
 <loc>https://www.weblime.com/stories/10-reasons-to-use-a-content-management-system/</loc>
 </url>
 <url>
+<loc>https://www.weblime.com/stories/how-to-market-a-home-renovation-business/</loc>
+</url>
+<url>
 <loc>https://www.weblime.com/stories/why-shopify-is-the-best-ecommerce-platform/</loc>
 </url>
 <url>
