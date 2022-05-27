ceef1086e4c6c98a25690ff77863c9fe56f8b701
Edan Ben-Atar <benatare@gmail.com>
2022-05-27 00:32:49 -0400

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -5,19 +5,32 @@ import pages from './src/utils/routelist.js';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+  preprocess: preprocess(),
+
   kit: {
-    // hydrate <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    adapter: vercel(),
+    // adapter: cloudflare(),
+    // prerender: {
+    //   default: true,
+    // },
+    // vite: {
+    //   plugins: [globals(), builtins()],
+    // },
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false,
+    }),
     prerender: {
-      crawl: true,
-      enabled: true,
-      entries: [...pages, '/sitemap.xml'],
+      default: true,
     },
   },
 };
