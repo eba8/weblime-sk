6a1e15ca44e019ec43d182ba4eef0c8f0ec78515
Edan Ben-Atar <benatare@gmail.com>
2022-05-26 23:39:43 -0400

    add vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapterStatic from '@sveltejs/adapter-static';
+// import adapterStatic from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import pages from './src/utils/routelist.js';
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +13,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapterStatic(),
+    adapter: vercel(),
     prerender: {
       crawl: true,
       enabled: true,
