9f03c21b669a4f8f125d1dc2eb2953cfabf2b8b7
Edan Ben-Atar <benatare@gmail.com>
2022-05-27 12:52:44 -0400

    update plausible

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,13 +10,6 @@
     defer
     data-domain="weblime.com"
     src="https://plausible.io/js/plausible.js"></script>
-  <script>
-    window.plausible =
-      window.plausible ||
-      function () {
-        (window.plausible.q = window.plausible.q || []).push(arguments);
-      };
-  </script>
 </svelte:head>
 
 <Header />
--- src/routes/get-in-touch.svelte
+++ src/routes/get-in-touch.svelte
@@ -60,6 +60,14 @@
 
     gtag('config', 'AW-663674682');
   </script>
+
+  <script>
+    window.plausible =
+      window.plausible ||
+      function () {
+        (window.plausible.q = window.plausible.q || []).push(arguments);
+      };
+  </script>
 </svelte:head>
 
 <main class="mx-auto">
@@ -70,16 +78,16 @@
         class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl"
       >
         <span class="xl:inline">Got a project </span>
-        <span class="px-3 text-primary-500 bg-gray-900 xl:inline">in mind?</span
+        <span class="bg-gray-900 px-3 text-primary-500 xl:inline">in mind?</span
         >
       </h1>
     </div>
   </div>
 
-  <div class="max-w-2xl px-4 pb-20 mx-auto mt-10 transform sm:px-6 lg:px-8">
+  <div class="mx-auto mt-10 max-w-2xl transform px-4 pb-20 sm:px-6 lg:px-8">
     <h2 class="mb-2">Let's talk!</h2>
     <p
-      class="mx-auto mb-4 text-gray-600 sm:text-lg md:mt-5 md:text-base md:max-w-3xl"
+      class="mx-auto mb-4 text-gray-600 sm:text-lg md:mt-5 md:max-w-3xl md:text-base"
     >
       If there's something we can help you with, just let us know. We'll be more
       than happy to offer you our help.
@@ -93,7 +101,7 @@
       <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
         <div class="sm:col-span-6">
           <input
-            class="w-full px-4 py-2 border border-gray-200 rounded-md border-1"
+            class="border-1 w-full rounded-md border border-gray-200 px-4 py-2"
             type="text"
             name="name"
             placeholder="Name"
@@ -103,7 +111,7 @@
         </div>
         <div class="sm:col-span-3">
           <input
-            class="w-full px-4 py-2 border border-gray-200 rounded-md border-1"
+            class="border-1 w-full rounded-md border border-gray-200 px-4 py-2"
             type="email"
             name="eml"
             placeholder="Email"
@@ -113,7 +121,7 @@
         </div>
         <div class="sm:col-span-3">
           <input
-            class="w-full px-4 py-2 border border-gray-200 rounded-md border-1"
+            class="border-1 w-full rounded-md border border-gray-200 px-4 py-2"
             type="tel"
             name="phone"
             placeholder="Phone"
@@ -123,7 +131,7 @@
         </div>
         <div class="sm:col-span-6">
           <textarea
-            class="w-full px-4 py-2 border border-gray-200 rounded-md border-1"
+            class="border-1 w-full rounded-md border border-gray-200 px-4 py-2"
             name="message"
             cols="30"
             rows="5"
@@ -133,7 +141,7 @@
           />
         </div>
         <input
-          class="hidden w-full px-4 py-2 border border-gray-200 rounded-md border-1"
+          class="border-1 hidden w-full rounded-md border border-gray-200 px-4 py-2"
           type="tel"
           name="eml2"
           placeholder=""
@@ -141,14 +149,14 @@
         />
         <button
           type="submit"
-          class="sm:col-span-6 px-6 py-3 text-lg font-medium text-primary-500 rounded-md bg-gray-900 hover:bg-gray-800"
+          class="rounded-md bg-gray-900 px-6 py-3 text-lg font-medium text-primary-500 hover:bg-gray-800 sm:col-span-6"
         >
           Send your message
         </button>
         {#if display_message === true}
           <div
             transition:fade
-            class="sm:col-span-6 px-6 py-3 bg-primary-500 text-gray-800 text-center rounded-md
+            class="rounded-md bg-primary-500 px-6 py-3 text-center text-gray-800 sm:col-span-6
       "
           >
             <p>Your message has been sent!</p>
