57ef1aef704dc1cbb93b2c11cc139a4d0e6a8fea
Edan Ben-Atar <benatare@gmail.com>
2022-06-09 09:21:10 -0400

    cleanup json stories main

--- src/routes/stories/index.js
+++ src/routes/stories/index.js
@@ -6,28 +6,31 @@ const { GHOST_KEY, API_URL } = process.env;
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const get = async () => {
-    const api = new GhostContentAPI({
-        url: API_URL,
-        key: GHOST_KEY,
-        version: 'v4.0',
-    });
+  const api = new GhostContentAPI({
+    url: API_URL,
+    key: GHOST_KEY,
+    version: 'v4.0',
+  });
 
-    try {
-        const jsonPosts = await api.posts.browse({
-          page: 1
-        });
-    
-        return {
-          status: 200,
-          body: {
-            data: {
-                stories: jsonPosts,
-                nextPage: 2,
-                prevPage: 0,
-            }
-          },
-        };
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const jsonPosts = await api.posts.browse({
+      page: 1,
+      fields: 'id,title,excerpt, feature_image,custom_excerpt,slug',
+      formats: `plaintext`,
+    });
+    jsonPosts.forEach((jsonPosts) => delete jsonPosts.plaintext);
+
+    return {
+      status: 200,
+      body: {
+        data: {
+          stories: jsonPosts,
+          nextPage: 2,
+          prevPage: 0,
+        },
+      },
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
