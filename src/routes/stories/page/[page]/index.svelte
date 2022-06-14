3f30878c293b2e6babca3a68bff40807505a8730
Edan Ben-Atar <benatare@gmail.com>
2022-06-13 22:03:37 -0400

    npm update

--- src/routes/stories/page/[page]/index.svelte
+++ src/routes/stories/page/[page]/index.svelte
@@ -10,10 +10,11 @@
   export let nextPage;
   export let prevPage;
 
-  // let title = `Page ${$page} of ${stories.meta.pagination.pages} - Stories
-  //    by WebLim`;
+  let title = `Page ${$page.params.page || 1} of ${
+    stories.meta.pagination.pages
+  } - Stories by WebLime`;
 
-  let title = 'WebLime Stories';
+  // let title = 'WebLime Stories';
   let description =
     "Read WebLime's latest stories. All about web development and digital marketing.";
 
