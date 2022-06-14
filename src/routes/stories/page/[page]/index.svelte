eb3c8280841177a661b03dd9704159c9e1f1c9fb
Edan Ben-Atar <benatare@gmail.com>
2022-06-13 21:58:10 -0400

    remove title issue

--- src/routes/stories/page/[page]/index.svelte
+++ src/routes/stories/page/[page]/index.svelte
@@ -12,9 +12,8 @@
 
   // let title = `Page ${$page} of ${stories.meta.pagination.pages} - Stories
   //    by WebLim`;
-  let title = `Page ${$page.params.page || 1} of ${
-    stories.meta.pagination.pages
-  } - Stories by WebLime`;
+
+  let title = 'WebLime Stories';
   let description =
     "Read WebLime's latest stories. All about web development and digital marketing.";
 
